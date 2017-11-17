e043bd80d5b9dc809df4f8f49c37eb9f39a392a0
Joris ZEFIRINI <sonizef@gmail.com>
2017-11-17 09:33:18 +0100

    [ADD] customEmitter configurable static var

--- emitterSandbox/customEmitter.swift
+++ emitterSandbox/customEmitter.swift
@@ -14,7 +14,29 @@ class customEmitter : SKEmitterNode{
     
     //On déclare et initialise toutes nos variables statics
     //C-a-d tous les paremetres possibles de notre emitters
-    static var COLOR_DEB = UIColor.black
+    static var COLOR_DEB = UIColor.yellow
+    static var EMITTER_BIRTHRATE = 100
+    static var EMITTER_MAXIMUM = 0
+    static var LIFETIME_START = 1
+    static var LIFETIME_RANGE = 0
+    static var POSITION_RANGE_X = 0
+    static var POSITION_RANGE_Y = 0
+    static var POSITION_RANGE_Z = 0
+    static var ANGLE_START = 0
+    static var ANGLE_RANGE = 0
+    static var SPEED_START = 0
+    static var SPEED_RANGE = 0
+    static var ACCELERATION_X = 0
+    static var ACCELERATION_Y = 0
+    static var ALPHA_START = 0
+    static var ALPHA_RANGE = 0
+    static var ALPHA_SPEED = 0
+    static var SCALE_START = 0
+    static var SCALE_RANGE = 0
+    static var SCALE_SPEED = 0
+    static var ROTATION_START = 0
+    static var ROTATION_RANGE = 0
+    static var ROTATION_SPEED = 0
     
     override init() {
         super.init()
