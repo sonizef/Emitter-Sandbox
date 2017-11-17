e9d7996f4dc98a765c68af0330abc8f6a23f6e5a
Joris ZEFIRINI <sonizef@gmail.com>
2017-11-17 14:22:28 +0100

    [ADD] Blur background for menu & MenuViewController

---
+++ emitterSandbox/MenuViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MenuViewController.swift
+//  emitterSandbox
+//
+//  Created by Joris ZEFIRINI on 17/11/2017.
+//  Copyright © 2017 SoniWeb. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.clear
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
